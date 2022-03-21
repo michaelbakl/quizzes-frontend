ec155c99a1403518eca9a7ce4f3422945d6302ae
Michael Baklykov <baklykovmichael@gmail.com>
2022-03-21 20:24:37 +0600

    [+] 07-base-practice react app added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button/button";
 
 function App() {
   return (
@@ -17,6 +18,11 @@ function App() {
         >
           Learn React
         </a>
+        <Button className='button' onClick={() => { alert('First button clicked!')}}>Click me to open an alert</Button>
+        <Button className='button' onClick={() => { alert('Second button clicked!')}} disabled >
+          Click me to open an alert
+        </Button>
+        <Button className='button' active href='test' >Click me to open an alert</Button>
       </header>
     </div>
   );
---
+++ src/components/Button/button.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './button.css';
+
+
+const Button = ({
+                  children, onClick, className, disabled, active, ...atrs
+                }) => {
+
+  const onClickAction = e => {
+    if (disabled) {
+      e.preventDefault();
+    } else {
+      return onClick(e);
+    }
+  };
+
+  const classes = classNames(
+    'btn',
+    className,
+    { active },
+  );
+
+  const Tag = atrs.href ? 'a' : 'button';
+
+  return (
+    <Tag
+      { ...atrs }
+      className={classes}
+      disabled={disabled}
+      onClick={onClickAction}
+    >{ children }</Tag>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  active: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  children: 'Default button',
+  onClick: () => {},
+  className: '',
+  disabled: false,
+};
+
+
+export default Button;
