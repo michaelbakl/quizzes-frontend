70abcd00bf7d62dd96e75dfd1e5d65dc37724223
Michael Baklykov <baklykovmichael@gmail.com>
2022-03-21 20:40:34 +0600

    [+] 07-base-practice .eslint.json added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Button from "./components/Button/button";
 
 function App() {
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div className="App">
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
+        <img src={logo} className="App-logo" alt="logo"/>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <p>
+          {/* eslint-disable-next-line react/react-in-jsx-scope */}
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,11 +24,18 @@ function App() {
         >
           Learn React
         </a>
-        <Button className='button' onClick={() => { alert('First button clicked!')}}>Click me to open an alert</Button>
-        <Button className='button' onClick={() => { alert('Second button clicked!')}} disabled >
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
+        <Button className='button' onClick={() => {
+          alert('First button clicked!')
+        }}>Click me to open an alert</Button>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
+        <Button className='button' onClick={() => {
+          alert('Second button clicked!')
+        }} disabled>
           Click me to open an alert
         </Button>
-        <Button className='button' active href='test' >Click me to open an alert</Button>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
+        <Button className='button' active href='test'>Click me to open an alert</Button>
       </header>
     </div>
   );
