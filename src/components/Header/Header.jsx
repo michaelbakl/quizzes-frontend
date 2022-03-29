875b75c69f5166a9acccc8f08c855a4586124c12
Michael Baklykov <baklykovmichael@gmail.com>
2022-03-29 11:56:23 +0600

    [+] 07-homework added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import './style.css';
-import Button from '../Button/Button';
+import { Button } from 'antd';
 
 function Header() {
   return (
     <header className="header">
       <div className="header__logo">7 quizzes</div>
       <div className="header__info-block">
-        <Button className="header__button">Game rules</Button>
+        <Button type="primary" shape="round">Game rules</Button>
         <span className="header__info-text">Points: 0</span>
       </div>
     </header>
