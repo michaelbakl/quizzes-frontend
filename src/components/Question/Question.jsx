9c959eb8a7fba8f2b2e0e214efc60915d9e13be3
Michael Baklykov <baklykovmichael@gmail.com>
2022-04-28 00:01:02 +0600

    [+] 10-workshop added multiple question support, new favicon

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -5,9 +5,12 @@ import PropTypes from 'prop-types';
 import './style.css';
 import { Button, Radio, Space } from 'antd';
 import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { getQuestion } from '../../actions/question/actions';
 
 function Question(props) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [counter, setCounter] = useState(1);
 
   const [answer, setAnswer] = useState(null);
@@ -19,35 +22,46 @@ function Question(props) {
     </Radio>
   ));
 
+  const MAX = 3;
+
   const onClick = () => {
     setCounter(counter + 1);
-    navigate('/game-finish');
+    if (counter < MAX) {
+      dispatch(getQuestion(counter + 1));
+      setDisabled(true);
+    } else {
+      navigate('/game-finish');
+    }
   };
 
   return (
-    <div className="question">
-      <div className="question__content">
-        <header className="question__title">
-          {`Question № ${counter}`}
-        </header>
-        <p className="question__text">
-          {props.question.questionText}
-        </p>
-      </div>
-      <div className="question__answers">
-        <Radio.Group
-          className="question__answers__radio"
-          onChange={e => {
-            setAnswer(e.target.value);
-            setDisabled(false);
-          }}
-          value={answer}
-          size="large"
-        >
-          <Space direction="vertical">
-            {answers}
-          </Space>
-        </Radio.Group>
+    <div>
+      <div className="question-wrapper">
+        <div className="question-block">
+          <div className="question__content">
+            <header className="question__title">
+              {`Question № ${counter}`}
+            </header>
+            <p className="question__text">
+              {props.question.questionText}
+            </p>
+          </div>
+          <div className="question__answers">
+            <Radio.Group
+              className="question__answers__radio"
+              onChange={e => {
+                setAnswer(e.target.value);
+                setDisabled(false);
+              }}
+              value={answer}
+              size="large"
+            >
+              <Space direction="vertical">
+                {answers}
+              </Space>
+            </Radio.Group>
+          </div>
+        </div>
       </div>
       <Button
         type="primary"
